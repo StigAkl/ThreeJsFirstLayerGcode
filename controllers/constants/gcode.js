fa03c1b51a16a7ab40ec0432c68161a1ed08fc1a
StigAkl <stg@hotmail.no>
2019-09-10 22:38:36 +0200

    Can find contours

--- controllers/constants/gcode.js
+++ controllers/constants/gcode.js
@@ -1,3 +1,6 @@
 module.exports = {
-    LinearMovement: "G1"
+    LinearMovement: "G1",
+    "Perimeter": "perimeter",
+    "Infill" : "infill"
+    
 }
--- controllers/stripGcodeController.js
+++ controllers/stripGcodeController.js
@@ -1,28 +1,87 @@
 const fs = require("fs"); 
 const pathToGcode = "./app/gcode/"; 
 const gcode = require("./constants/gcode"); 
+const fileName = "octopus.gcode"; 
+getLayerHeight = () => {
+
+    let layerHeight = undefined; 
+    let layerHeightFound = false; 
+    fs.readFileSync(pathToGcode + fileName, "utf-8").split(/\r?\n/).forEach(line=>{
+        if(line.includes("first_layer_height")) {
+            if(!layerHeightFound) {
+                lineSplit = line.split(" = "); 
+                layerHeight = parseFloat(lineSplit[lineSplit.length-1]);
+                layerHeightFound = true; 
+            }
+        }
+    }) 
+
+    return layerHeight; 
+
+}
 
 exports.gcodeController = (req, res) => {
 
-    const points = []; 
+    const linearPoints = []; 
+    const perimeterPoints = [];
+    const infillPoints = []; 
 
-    fs.readFileSync(pathToGcode + "square.gcode", 'utf-8').split(/\r?\n/).forEach(function(line){
-        const split = line.split(" "); 
-        console.log(line); 
-        if(split[0] === gcode.LinearMovement) {
-            
-            split[1].startsWith("X") && split[2].startsWith("Y") 
-            ? 
-            points.push({
-                x: split[1].substring(1, split[1].length),
-                y: split[2].substring(1, split[2].length)
-            })
-            : 
-            console.log("Undefined"); 
+    const layerHeight = getLayerHeight(); 
+    
+    let isOnFirstLayer = true; 
+
+    fs.readFileSync(pathToGcode + fileName, 'utf-8').split(/\r?\n/).forEach(function(line){
+        const splitCode = line.split(" "); 
+        const splitComment = line.split(" ; "); 
+
+        if(isOnFirstLayer) {
+            if(splitComment.length > 1 && splitComment[1].includes("restore layer Z")) {
+                splitLayerRestoreLine = line.split(" "); 
+                console.log(splitLayerRestoreLine); 
+                if(splitLayerRestoreLine[1].startsWith("Z")) {
+                    let currentLayerHeight = parseFloat(splitLayerRestoreLine[1].substring(1, splitLayerRestoreLine[1].length)); 
+
+                    console.log(currentLayerHeight); 
+                    if(currentLayerHeight > layerHeight) {
+                        isOnFirstLayer = false; 
+                    }
+                }
+            }
+                if(splitCode.length && splitCode[0] === gcode.LinearMovement) {
+                    if(splitCode[1].startsWith("X") && splitCode[2].startsWith("Y") && splitCode[3].startsWith("E")) {
+
+                        var positiveExtrusion = parseFloat(splitCode[3].substring(1, splitCode[3].length)) > 0 ? true : false;
+
+                        if(positiveExtrusion) {
+                            switch(splitCode[splitCode.length-1]) {
+
+                                case gcode.Perimeter: 
+                                    perimeterPoints.push({
+                                        x: splitCode[1].substring(1, splitCode[1].length),
+                                        y: splitCode[2].substring(1, splitCode[2].length) 
+                                    })
+                                break;
+
+                                case gcode.Infill: 
+                                    infillPoints.push({
+                                        x: splitCode[1].substring(1, splitCode[1].length),
+                                        y: splitCode[2].substring(1, splitCode[2].length) 
+                                    })
+                            }
+
+                            linearPoints.push({
+                                x: splitCode[1].substring(1, splitCode[1].length),
+                                y: splitCode[2].substring(1, splitCode[2].length)
+                            })
+                        }
+                    }
+                }
+                
         }
-      }) 
+      })
+
       res.json({
-          numPoints: points.length,
-          points: points
+          numPoints: perimeterPoints.length,
+          points: perimeterPoints
       }); 
 }
